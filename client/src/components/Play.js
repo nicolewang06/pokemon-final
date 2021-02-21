adbca0d9a6b12ad25d4b65dbcee506c3bd0c7611
Nicole Wang <nicolewang06@gmail.com>
2021-02-21 15:10:11 -0500

    update

--- client/src/components/Play.js
+++ client/src/components/Play.js
@@ -8,21 +8,27 @@ class Play extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      details: [],
-      results: [],
-      score: [],
-      selected: []
+      correct: [],
+      randomNameOne: [],
+      randomNameTwo: [],
+      randomNameThree: []
     }
   }
 
-  
-
   async componentDidMount() {
     try {
+
       const random = Math.floor(Math.random() * 899)
+      const randomOne = Math.floor(Math.random() * 899)
+      const randomTwo = Math.floor(Math.random() * 899)
+      const randomThree = Math.floor(Math.random() * 899)
         const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${random}`);
-        const pNames = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=899');
-        this.setState({ details: res.data, testing: pNames.data })
+        //const pNames = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=899');
+        const randomNameOne = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomOne}`);
+        const randomNameTwo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomTwo}`);
+        const randomNameThree = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomThree}`);
+        this.setState({ correct: res.data, randomNameOne: randomNameOne.data, randomNameTwo: randomNameTwo.data, randomNameThree: randomNameThree.data })
+
     } catch(err) {
         console.error(err.message);
     }
@@ -30,19 +36,19 @@ class Play extends React.Component {
 
   async guess(e) {
     const answer = document.querySelector('#playImage');
-    const allCaps = this.state.details.name.toUpperCase();
+    const allCaps = this.state.correct.name.toUpperCase();
     
-    if (this.state.details.name === e.target.value) {
+    if (this.state.correct.name === e.target.value) {
       answer.style.filter = "brightness(100%)";
       alert("💥 You caught " + allCaps + " !! 🎉\n\nCheck out the Pokemon page to give your new friend a nickname \n\n\n-- refresh page to encounter another Pokemon --");
 
       const res = await axios.post('http://localhost:8080/pokemon', {
-        pokemonNum: this.state.details.id,
-        pokemonName: this.state.details.name,
-        imageUrl: `${img}${this.state.details.id}`
+        pokemonNum: this.state.correct.id,
+        pokemonName: this.state.correct.name,
+        imageUrl: `${img}${this.state.correct.id}`
       });
     } else {
-      alert("nope \nit got away");
+      alert("nope");
       this.componentDidMount();
       answer.style.filter = "brightness(0%)";
     }
@@ -53,34 +59,43 @@ class Play extends React.Component {
             <div className="playContainer">
               <div id="playContent">
                 <div className="playTitle">&nbsp; Who's that Pok&#233;mon ? &nbsp;</div><br></br>
-                  <div id="playImage"><img src={img + this.state.details.id + ".png"} alt="" width="250px" /></div><br></br>
+                  <div id="playImage"><img src={img + this.state.correct.id + ".png"} alt="" width="250px" /></div><br></br>
                       <div id="nameContainer">
                       <div className='row'>
                           <div className='column'>
-                            <input type="button" className='playName' onClick={(e) => this.guess(e, 'value')} value={this.state.details.name} key={this.state.details.id}/>
+                            <input type="button" className='playName' onClick={(e) => this.guess(e, 'value')} value={this.state.correct.name} key={this.state.correct.id}/>
                           </div>
                           <div className='column'>
-                            <input type="button" className='playName' onClick={(e) => this.guess(e, 'value')} value="pikachu" key="a"/>
+                            <input type="button" className='playName' onClick={(e) => this.guess(e, 'value')} value={this.state.randomNameOne.name} key={this.state.randomNameOne.id}/>
                           </div>
 
                         </div>
                         <div className='row'>
                         <div className='column'>
-                            <input type="button" className='playName' onClick={(e) => this.guess(e, 'value')} value="eevee" key="b"/>
+                            <input type="button" className='playName' onClick={(e) => this.guess(e, 'value')} value={this.state.randomNameTwo.name} key={this.state.randomNameTwo.naidme}/>
                           </div>
                           <div className='column'>
-                            <input type="button" className='playName' onClick={(e) => this.guess(e, 'value')} value="snorlax" key="c"/>
+                            <input type="button" className='playName' onClick={(e) => this.guess(e, 'value')} value={this.state.randomNameThree.name} key={this.state.randomNameThree.id}/>
                           </div>
 
                         </div>
                       </div>
                       <div id="hintTypeContainer">
-                        <div id="hintType">{this.state.details.types && this.state.details.types.map(type => {
+                        <div id="hintType">{this.state.correct.types && this.state.correct.types.map(type => {
                             return (<div>{type.type.name} type</div>); })}
                         </div>
                         mouse over to show hint
                       </div>
               </div>
+
+        <div>
+          <ul>
+            {/* {this.state.nameAndUrl.map(item => (
+              <li key={item}>{item}</li>
+            ))} */}
+          </ul>
+        </div>
+
             </div>
         )
     }
--- client/src/components/Pokemon.js
+++ client/src/components/Pokemon.js
@@ -8,6 +8,8 @@ class Pokemon extends Component {
       super(props);
   
       this.state = { data: [], isEditToggled: false }
+      this.deletePokemon = this.deletePokemon.bind(this)
+      this.handleEditSubmit = this.handleEditSubmit.bind(this)
     }
 
     async componentDidMount() {
@@ -27,35 +29,27 @@ class Pokemon extends Component {
           //alert("we'll meet again");
           const updateRes = await axios.get('http://localhost:8080/pokemon/');
             this.setState({ data: updateRes.data});
+            console.log(this.state.data)
         } catch(e) {
           console.error(e, e.message);
         }
       }
 
-
-
-      //const [selectedPokemon, setSelectedPokemon] = useState(null);
-
-
-      
-      // handleChange(e) {
-      //   const { name, value } = e.target;
-      //   this.setState( {...this.state,  [name]: value })
-      // }
-
       async handleEditSubmit(e) {
         //alert("refresh page to see changes")
         //e.preventDefault();
-        let edit = document.getElementById("myDIV");
-        let name = document.getElementById("pokemonName");
         const nickname = document.getElementById("nickname").value;
         const editPokemon = {...this.state.data[0], nickname: nickname };
+        //
+        // ------------fix update to toggle off edit
+        // when editing 2nd , 1st changes (array thing)
+        //
+
         try {
             const res = await axios.patch('http://localhost:8080/pokemon/', editPokemon);
             const updateRes = await axios.get('http://localhost:8080/pokemon/');
-            this.setState({ data: updateRes.data});
-            edit.style.display = "none";
-            name.style.display = "block";
+            this.setState({ data: updateRes.data, isEditToggled: false});
+            window.location.reload(false);
           } catch(e) {
             console.error(e, e.message);
           }
@@ -68,7 +62,7 @@ class Pokemon extends Component {
             <div className="pokemonCardContainer">  
               {
               this.state.data && this.state.data.map(data => (
-                  <SinglePokemon data = {data} deletePokemon = {this.deletePokemon} />
+                  <SinglePokemon data = {data} deletePokemon = {this.deletePokemon} handleEditSubmit = {this.handleEditSubmit }/>
                   
               ))
               }
@@ -78,6 +72,9 @@ class Pokemon extends Component {
     }
 }
 
+
+
+
 class SinglePokemon extends Component{
   constructor(props){
     super(props)
@@ -85,46 +82,42 @@ class SinglePokemon extends Component{
     this.state = {isEditToggled: false}
   }
 
-
   async toggleEdit() {
     // let edit = document.getElementById("myDIV");
     // let name = document.getElementById("pokemonName");
     // edit.style.display = "block";
     // name.style.display = "none";
     
-    this.setState({isEditToggled: true})
-
-
+    !this.state.isEditToggled ? this.setState({isEditToggled: true}) : this.setState({isEditToggled: false});
   }
-
 
   render(){
     return(
 
       <div className="pokemonCard" key={this.props.data.id}>  
-                      <div className="pokemonBackground" >
-                          <form><div id="delete" onClick={ (e) => this.props.deletePokemon(this.props.data.id) }>❌</div></form>
-                          <img id="pokemonImage" src= {this.props.data.imageUrl + ".png"} alt="" width="150px" />
-                          <div id="pokemonNum">#{this.props.data.pokemonNum}</div>
-                      </div> 
+        <div className="pokemonBackground" >
+            <form><div id="delete" onClick={ (e) => this.props.deletePokemon(this.props.data.id) }>❌</div></form>
+            <img id="pokemonImage" src= {this.props.data.imageUrl + ".png"} alt="" width="150px" />
+            <div id="pokemonNum">#{this.props.data.pokemonNum}</div>
+        </div> 
 
-                      <div className="pokemonContent">
-                          <div id="pokemonNameContent">
-                              <div id="edit" onClick={ (e) => this.toggleEdit() }>
-                                ✏️
-                              </div>
-                              { this.state.isEditToggled && <div id="myDIV">
-                                <form onSubmit={ (e) => this.handleEditSubmit(e) }>
-                                  <input id="nickname" type="text" size="10" defaultValue={this.props.data.pokemonName}/>
-                                  <input type="submit" value="update" />
-                                </form>
-                              </div>}
-                             { !this.state.isEditToggled && <div id="pokemonName">
-                                  {this.props.data.nickname ? this.props.data.nickname : this.props.data.pokemonName}
-                              </div>}
-                          </div>
-                      </div>
-                  </div>
+        <div className="pokemonContent">
+            <div id="pokemonNameContent">
+                <div id="edit" onClick={ (e) => this.toggleEdit() }>
+                  ✏️
+                </div>
+                { this.state.isEditToggled && <div id="myDIV">
+                  <form onSubmit = { this.props.handleEditSubmit } >
+                    <input id="nickname" type="text" size="10" defaultValue={this.props.data.pokemonName}/>
+                    <input type="submit" value="update"/>
+                  </form>
+                </div>}
+                { !this.state.isEditToggled && <div id="pokemonName">
+                    {this.props.data.nickname ? this.props.data.nickname : this.props.data.pokemonName}
+                </div>}
+            </div>
+        </div>
+    </div>
 
     )
   }
