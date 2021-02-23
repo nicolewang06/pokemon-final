0afccc5ae74f442c035c90137a73445b9aea7d78
Nicole Wang <nicolewang06@gmail.com>
2021-02-23 17:31:11 -0500

    update

--- client/src/components/Pokemon.js
+++ client/src/components/Pokemon.js
@@ -5,8 +5,8 @@ import SinglePokemon from './SinglePokemon'
 
 
 //const img = "https://pokeres.bastionbot.org/images/pokemon/"
-const postgresDB = "http://localhost:8080/pokemon"
-const deploySite = "https://fast-falls-09879.herokuapp.com/pokemon/"
+const deploySite = "http://localhost:8080/pokemon"
+// const deploySite = "https://fast-falls-09879.herokuapp.com/pokemon/"
 
 
 class Pokemon extends Component {
